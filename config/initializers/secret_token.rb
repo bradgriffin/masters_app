8f957823ebce76d34974d8a9bb3e76a67588ae48
Brad Griffin <bgriffin20@gmail.com>
2012-02-13 21:55:28 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MastersApp::Application.config.secret_token = 'd8407284e0305c05a18675e535e59b8fb4d8525a939cd0dd8297ceea889d6450aa4736add87b37e617cf248575c4818947951f20cffb2e4020b747dcd3fb20db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MastersApp::Application.config.session_store :cookie_store, key: '_masters_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MastersApp::Application.config.session_store :active_record_store
