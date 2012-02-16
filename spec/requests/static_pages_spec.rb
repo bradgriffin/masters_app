aea54a7c7636ac3bf6fe713f2068cf1c1725a7ca
Brad Griffin <bgriffin20@gmail.com>
2012-02-15 19:38:23 -0600

    Static Pages complete

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,12 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Masters Challenge')
     end
+
+    it "should have the right title" do
+    visit '/static_pages/home'
+    page.should have_selector('title',
+                    :text => "Masters Challenge | Home")
+    end
   end
 
   describe "Help page" do
@@ -16,13 +22,25 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "should have the right title" do
+    visit '/static_pages/help'
+    page.should have_selector('title',
+                    :text => "Masters Challenge | Help")
+    end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the content 'About Me'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_content('About Me')
+    end
+
+    it "should have the right title" do
+    visit '/static_pages/about'
+    page.should have_selector('title',
+                    :text => "Masters Challenge | About Me")
     end
   end
 end
