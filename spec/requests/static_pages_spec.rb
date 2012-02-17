3bb8c35120e6757cf346cac9cd197dcf2e930f52
Brad Griffin <bgriffin20@gmail.com>
2012-02-16 22:29:42 -0600

    Added new logo, rendered layouts, removed public/index

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,12 +5,12 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Masters Challenge'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Masters Challenge')
     end
 
     it "should have the right title" do
-    visit '/static_pages/home'
+    visit root_path
     page.should have_selector('title',
                     :text => "Masters Challenge | Home")
     end
@@ -19,12 +19,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
 
     it "should have the right title" do
-    visit '/static_pages/help'
+    visit help_path
     page.should have_selector('title',
                     :text => "Masters Challenge | Help")
     end
@@ -33,14 +33,28 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Me'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Me')
     end
 
     it "should have the right title" do
-    visit '/static_pages/about'
+    visit about_path
     page.should have_selector('title',
                     :text => "Masters Challenge | About Me")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Masters Challenge | Contact")
     end
   end
 end
