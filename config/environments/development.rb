52ed579f29cce396b87353c29f045513745797a1
Brad Griffin <bgriffin20@gmail.com>
2012-02-21 21:10:39 -0600

    Added configs to development so that mailing will log appropriately.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,9 @@ MastersApp::Application.configure do
 
   #Define default url options for Devise to work correctly
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  # A dummy setup for development - no deliveries, but logged
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 end
