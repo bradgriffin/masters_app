5da327d8d8bdb3e578f736edf6dd82faa8f16162
Brad Griffin <bgriffin20@gmail.com>
2012-02-23 20:36:58 -0600

    Added gravatar to users profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,6 @@ before_filter :set_golfers
 		@golfers = Golfer.all
 	end
 
-	# Return a title on a per-page basis.
-  	def title
-   	 base_title = "Masters Challenge"
-    	if @title.nil?
-      		base_title
-    	else
-      	"#{base_title} | #{@title}"
-    	end
-  	end
-
 protect_from_forgery
   def after_sign_in_path_for(resource)
         stored_location_for(resource) || resource
